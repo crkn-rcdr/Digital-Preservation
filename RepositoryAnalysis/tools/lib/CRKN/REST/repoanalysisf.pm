4536dfabf54e992db2469c661765bd905d258933
Russell McOrmond <irmcormond@crkn.ca>
2019-10-17 13:47:34 -0400

    Tool to copy jhove xml reports from 'filemeta' database to 'repoanalysis' swift container. https://github.com/crkn-rcdr/CIHM-Meta/issues/4

---
+++ RepositoryAnalysis/tools/lib/CRKN/REST/repoanalysisf.pm
@@ -0,0 +1,78 @@
+package CRKN::REST::repoanalysisf;
+
+use strict;
+use Carp;
+use DateTime;
+use JSON;
+
+use Moo;
+with 'Role::REST::Client';
+use Types::Standard qw(HashRef Str Int Enum HasMethods);
+
+
+=head1 NAME
+CRKN::REST::repoanalysisf - Subclass of Role::REST::Client used to
+interact with "repoanalysisf" CouchDB databases
+=head1 SYNOPSIS
+    my $fmeta = CRKN::REST::repoanalysisf->new($args);
+      where $args is a hash of arguments.  In addition to arguments
+      processed by Role::REST::Client we have the following 
+      $args->{database} is the Couch database name.
+=cut
+
+sub BUILD {
+    my $self = shift;
+    my $args = shift;
+
+    $self->{LocalTZ} = DateTime::TimeZone->new( name => 'local' );
+    $self->database($args->{database});
+    $self->set_persistent_header('Accept' => 'application/json');
+}
+
+has 'database' => (
+    isa => Str,
+    is  => 'rw',
+    default => ''
+);
+
+=head1 METHODS
+=head2 update_basic
+    sub update_basic ( string UID, hash updatedoc )
+    updatedoc - a hash that is passed to the _update function of the
+        design document to update data for the given UID.
+        Meaning of fields in updatedoc is defined by that function.
+  returns null, or a string representing the return from the _update
+  design document.  Return values include "update", "no update", "no create".
+=cut
+
+sub update_basic {
+  my ($self, $uid, $updatedoc) = @_;
+  my ($res, $code, $data);
+
+  # This encoding makes $updatedoc variables available as form data
+  $self->type("application/x-www-form-urlencoded");
+  $res = $self->post("/".$self->{database}."/_design/tdr/_update/basic/".$uid, $updatedoc, {deserializer => 'application/json'});
+
+  if ($res->code != 201 && $res->code != 200) {
+      warn "_update/basic/$uid POST return code: " . $res->code . "\n";
+  }
+  if ($res->data) {
+      return $res->data->{return};
+  }
+}
+
+sub get_file {
+    my ($self, $file) = @_;
+
+    $self->type("application/json");
+    my $res = $self->get("/".$self->{database}."/$file",{}, {deserializer => 'application/json'});
+    if ($res->code == 200) {
+        return $res->data;
+    }
+    else {
+        warn "get_aip return code: ".$res->code."\n"; 
+        return;
+    }
+}
+
+1;
