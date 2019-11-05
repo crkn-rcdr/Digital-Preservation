3ab608e1943f016ed96ad93559e8f110e5fa893f
Russell McOrmond <rmcormond@crkn.ca>
2019-11-05 12:38:16 -0500

    View no longer used

--- RepositoryAnalysis/tools/couchdb/repoanalysis/design/filemap/views/md5sizesip.js
+++
@@ -1,8 +0,0 @@
-module.exports.map = function(doc) {
-    if ("sipfiles" in doc) {
-	Object.keys(doc["sipfiles"]).forEach(function(file) {
-	    emit(doc["sipfiles"][file],file);
-	});
-    };
-};
-module.exports.reduce = "_count";
