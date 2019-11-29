d0fd9750dd214b17eb0cd32031a9ff8a24c6814e
Russell McOrmond <irmcormond@crkn.ca>
2019-11-29 15:25:22 -0500

    New walkmetadata tool, currently only basic processing of changelog.txt files.

---
+++ RepositoryAnalysis/tools/lib/CRKN/Repository/walkmetadata/Process.pm
@@ -0,0 +1,156 @@
+package CRKN::Repository::walkmetadata::Process;
+
+use strict;
+use Data::Dumper;
+use XML::LibXML;
+use Archive::Zip qw( :ERROR_CODES :CONSTANTS );
+use File::Basename;
+use File::Temp qw/ tempfile tempdir /;
+
+sub new {
+    my($class, $args) = @_;
+    my $self = bless {}, $class;
+
+    if (ref($args) ne "HASH") {
+        die "Argument to CRKN::Repository::walkmetadata::Process->new() not a hash\n";
+    };
+    $self->{args} = $args;
+
+    if (!$self->log) {
+        die "Log::Log4perl object parameter is mandatory\n";
+    }
+    if (!$self->worker) {
+        die "worker object parameter is mandatory\n";
+    }
+    if (!$self->swift) {
+        die "swift object parameter is mandatory\n";
+    }
+    if (!$self->swiftcontainer) {
+        die "swiftcontainer parameter is mandatory\n";
+    }
+    if (!$self->repoanalysis) {
+        die "repoanalysis object parameter is mandatory\n";
+    }
+    if (!$self->aip) {
+        die "Parameter 'aip' is mandatory\n";
+    }
+    $self->{updatedoc} = {};
+
+    return $self;
+}
+
+sub args {
+    my $self = shift;
+    return $self->{args};
+}
+sub cmdargs {
+    my $self = shift;
+    return $self->args->{args};
+}
+sub aip {
+    my $self = shift;
+    return $self->args->{aip};
+}
+sub worker {
+    my $self = shift;
+    return $self->args->{worker};
+}
+sub log {
+    my $self = shift;
+    return $self->args->{log};
+}
+sub swift {
+    my $self = shift;
+    return $self->args->{swift};
+}
+sub swiftcontainer {
+    my $self = shift;
+    return $self->args->{swiftcontainer};
+}
+sub repoanalysis {
+    my $self = shift;
+    return $self->args->{repoanalysis};
+}
+sub repodoc {
+    my $self = shift;
+    return $self->{repodoc};
+}
+
+sub process {
+    my ($self)= @_;
+
+    my $aip=$self->aip;
+
+    my $tempdir= File::Temp->newdir();
+
+    $self->loadRepoDoc();
+    $self->worker->setResult("manifestdate",$self->repodoc->{reposManifestDate});
+
+    my $changelogfile = $self->aip."/data/changelog.txt";
+    my $r = $self->swift->object_get($self->swiftcontainer,$changelogfile);
+    if ($r->code != 200) {
+	die "object_get container: '".$self->swiftcontainer."' , object: '$changelogfile'  returned ". $r->code . " - " . $r->message. "\n";
+    }
+    my @changelog=split /\n/, $r->content;
+
+    my @changes;
+    foreach my $logline (@changelog) {
+	if ($logline =~ /^(\d\d\d\d\-\d\d-\d\dT\d\d:\d\d:\d\dZ)\s+(.*)$/) {
+	    my %change;
+	    $change{'date'}=$1;
+	    my $line=$2;
+	    for ($line) {
+		if (/^Created new AIP$/) {
+		    $change{'operation'}='new';
+		}
+		elsif (/^Built CMR record$/) {
+		    $change{'operation'}='cmr';
+		}
+		elsif (/^Created new SIP in existing AIP$/) {
+		    $change{'operation'}='updatesip';
+		}
+		elsif (/^Updated SIP; old SIP stored as revision\s+(\w+)\s*$/) {
+		    $change{'operation'}='updatesip';
+		    $change{'revision'}=$1;
+		}
+		elsif (/^Updated metadata record; old record stored in revision\s+(\w+)\s*$/) {
+		    $change{'operation'}='mdupdate';
+		    $change{'revision'}=$1;
+		}
+		elsif (/^Updated metadata record. Reason:\s+(.+)\s*$/) {
+		    $change{'operation'}='mdupdate';
+		    $change{'reason'}=$1;
+		}
+		elsif (/^Deleted SIP and all revisions from archive. Reason:\s+(.+)\s*$/) {
+		    $change{'operation'}='delsip';
+		    $change{'reason'}=$1;
+		}
+		else {
+		    $change{'line'}=$line;
+		    print $self->aip." line: $line\n";
+		}
+	    }
+	    push @changes, \%change;
+	} else {
+	    warn "Log line didn't start with date: $logline\n";
+	}
+    }
+    $self->worker->setResult("changes",\@changes);
+}
+
+# Load document from 'repoanalysis' for this AIP
+sub loadRepoDoc {
+    my $self = shift;
+
+    $self->repoanalysis->type("application/json");
+    
+    my $res = $self->repoanalysis->get("/".$self->repoanalysis->database."/".$self->aip,{}, {deserializer => 'application/json'});
+    if ($res->code == 200) {
+	$self->{repodoc}=$res->data;
+    } else {
+        die "Can't get repoanalysis document. GET return code: ".$res->code."\n";
+    }
+}
+
+
+1;
