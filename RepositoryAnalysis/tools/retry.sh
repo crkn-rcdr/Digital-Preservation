66c652c832fd88834fd1dc228a077aa68ce41f33
Russell McOrmond <rmcormond@crkn.ca>
2020-07-31 13:29:13 -0400

    Other things we have JHove retrying.

--- RepositoryAnalysis/tools/retry.sh
+++ RepositoryAnalysis/tools/retry.sh
@@ -4,8 +4,18 @@
 RAF='http://iris.tor.c7a.ca:5984/repoanalysisf'
 RA='http://iris.tor.c7a.ca:5984/repoanalysis'
 
-# Loop through all approved document AIPs
-curl -s -X GET "$RAF/_design/ra/_view/stats?reduce=false&include_docs=false&startkey=\[\"Well-Formed%2C%20but%20not%20valid\",\"XML\"\]&endkey=\[\"Well-Formed%2C%20but%20not%20valid\",\"XML\",\{\}\]"  |
+# Loop through AIPs
+
+# Not valid XML
+# curl -s -X GET "$RAF/_design/ra/_view/stats?reduce=false&include_docs=false&startkey=\[\"Well-Formed%2C%20but%20not%20valid\",\"XML\"\]&endkey=\[\"Well-Formed%2C%20but%20not%20valid\",\"XML\",\{\}\]"  |
+
+#curl -s -X GET "$RAF/_design/ra/_view/stats?reduce=false&include_docs=false&startkey=\[\"Well-Formed%2C%20but%20not%20valid\",\"TIFF\"\]&endkey=\[\"Well-Formed%2C%20but%20not%20valid\",\"TIFF\",\{\}\]"  |
+
+curl -s -X GET "$RAF/_design/ra/_view/stats?reduce=false&include_docs=false&startkey=\[\"Not%20well-formed\",\"PDF\"\]&endkey=\[\"Not%20well-formed\",\"PDF\",\{\}\]"  |
+
+
+# Clean up succes that had error message.
+#curl -s -X GET "$RAF/_design/ra/_view/stats?reduce=false&include_docs=false&startkey=\[\"Well-Formed%20and%20valid\",\"JPEG\",\"jpg\",\"oocihm\",\"a\"\]&endkey=\[\"Well-Formed%20and%20valid\",\"JPEG\",\"jpg\",\"oocihm\",\{\}\]"  |
 
 # Convert "curl" output into simple AIP list
   grep '^{"id' | sed -e 's/^.*"id":"\([^"\/]*\).*$/\1/' | sort -u |
